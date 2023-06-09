afc47d8cddc1e67ce406f234f789276f04167d3e
NIHAD779 <nigad779@gmail.com>
2023-06-09 22:39:53 +0530

    Ui fixes

--- front-end/src/components/AddFile.jsx
+++ front-end/src/components/AddFile.jsx
@@ -4,20 +4,37 @@ import {
   ref,
   uploadBytesResumable,
   getDownloadURL,
-  deleteObject, // Add this import statement
+  deleteObject,
 } from "firebase/storage";
 import storage from "../firebase";
-import { IconFileFilled } from "@tabler/icons-react";
 import { Button } from "@mantine/core";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+import { IconFileFilled } from "@tabler/icons-react";
+import { IconBrandWhatsapp } from "@tabler/icons-react";
+
+// Set worker path for PDF.js
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const AddFile = () => {
   const [fileList, setFileList] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
   const [hoveredFile, setHoveredFile] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [isMobileOrTablet, setIsMobileOrTablet] = useState(false);
 
   useEffect(() => {
     fetchFiles();
+    checkMobileOrTablet();
+    window.addEventListener("resize", checkMobileOrTablet);
+    return () => window.removeEventListener("resize", checkMobileOrTablet);
   }, []);
+
+  const checkMobileOrTablet = () => {
+    const isMobileOrTablet =
+      window.innerWidth <= 1024 && window.innerHeight <= 1024;
+    setIsMobileOrTablet(isMobileOrTablet);
+  };
 
   const fetchFiles = async () => {
     try {
@@ -71,6 +88,10 @@ const AddFile = () => {
 
   const handleFileClick = (file) => {
     setSelectedFile(file);
+
+    if (isMobileOrTablet) {
+      window.open(file.url, "_blank");
+    }
   };
 
   const handleDeleteFile = async (filename) => {
@@ -86,27 +107,29 @@ const AddFile = () => {
     }
   };
 
-const handleshare = (file) => {
-  const message = `Check out this PDF: `;
-  const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(
-    `${message}[${file.filename}](${file.url})`
-  )}`;
-  const link = document.createElement("a");
-  link.href = whatsappUrl;
-  link.target = "_blank";
-  link.click();
-};
+  const handleJoinCommunity = () => {
+    // Add your logic to handle joining the Whatsapp community
+  };
 
+  const handleshare = (file) => {
+    const message = `Check out this PDF: `;
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(
+      `${message}[${file.filename}](${file.url})`
+    )}`;
+    const link = document.createElement("a");
+    link.href = whatsappUrl;
+    link.target = "_blank";
+    link.click();
+  };
 
-
-
-
-
-
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
 
   return (
-    <div>
-      <div className="items-center p-2 flex gap-2">
+    <div className="p-2">
+      <h1 className="text-center">EXAM SATHI</h1>
+      <div className="flex items-center gap-2 mb-4">
         <input
           type="file"
           onChange={handleFileChange}
@@ -120,72 +143,88 @@ const handleshare = (file) => {
           Upload!
         </Button>
       </div>
-      <div className="flex">
-        <div className="w-[45%] h-[100vh] grid grid-cols-3 gap-4 border">
-          {fileList.map((file) => (
-            <div
-              key={file.filename}
-              className="flex flex-col items-center relative"
-              onClick={() => handleFileClick(file)}
-              onMouseEnter={() => setHoveredFile(file)}
-              onMouseLeave={() => setHoveredFile(null)}
-            >
-              <IconFileFilled className="w-24 h-24" />
-              <span>{file.filename}</span>
-              <span className="truncate">
-                {(file.size / 1048576).toFixed(2)} MB
-              </span>
-              {hoveredFile === file && (
-                <div>
-                  <button
-                    className="absolute top-0 right-0 p-2 text-gray-500 hover:text-red-500"
-                    onClick={() => handleDeleteFile(file.filename)}
-                    aria-label="Delete file"
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-2/5">
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 border">
+            {fileList.map((file) => (
+              <div
+                key={file.filename}
+                className="flex flex-col items-center relative text-center p-2"
+                onClick={() => handleFileClick(file)}
+                onMouseEnter={() => setHoveredFile(file)}
+                onMouseLeave={() => setHoveredFile(null)}
+              >
+                <IconFileFilled className="w-24 h-24" />
+                <span>{file.filename}</span>
+                <button
+                  onClick={() => handleshare(file)}
+                  className="absolute top-0 right-0"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-brand-whatsapp"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="green"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      fill="red"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M18 6l-12 12" />
-                      <path d="M6 6l12 12" />
-                    </svg>
-                  </button>
-                  <button onClick={() =>handleshare(file)}>Share</button>{" "}
-                </div>
-              )}
-            </div>
-          ))}
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
+                    <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="w-[55%] m-2">
-          {selectedFile ? (
-            <div>
-              <h3>Selected File: {selectedFile.filename}</h3>
-              <embed
-                src={selectedFile.url}
-                width="100%"
-                height="900px"
-                type="application/pdf"
-                target="_blank"
-              />
-            </div>
-          ) : (
-            <div className="flex items-center justify-center h-full">
-              <h1>No Preview</h1>
-              <img src="/NoPreview.png" alt="No Preview" />
-            </div>
-          )}
+        {!isMobileOrTablet && (
+          <div className="w-full lg:w-3/5 mt-4 lg:mt-0">
+            {selectedFile ? (
+              <div>
+                <h3>Selected File: {selectedFile.filename}</h3>
+                <embed
+                  src={selectedFile.url}
+                  width="100%"
+                  height="900px"
+                  type="application/pdf"
+                  target="_blank"
+                />
+              </div>
+            ) : (
+              <div className="flex items-center justify-center h-full">
+                <h1>No Preview</h1>
+                <img src="/NoPreview.png" alt="No Preview" />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-[100%] md:w-[25%] relative bg-green-500 text-white p-4 rounded-lg shadow-lg md:left-auto m-1 md:top-auto md:transform-none md:fixed md:right-0 z-20">
+          <div className="flex items-center justify-center space-x-2">
+            <IconBrandWhatsapp size={20} />
+            <span>Join our Whatsapp Community</span>
+          </div>
+          <Button onClick={handleJoinCommunity} variant="light" size="sm">
+            Join Now
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-start space-x-2 bg-black bg-opacity-70 w-[100%] md:w-[50%] lg:w-[30%]   text-white p-4 rounded-lg shadow-lg md:fixed md:left-4 md:bottom-4 m-1">
+          <img
+            src="/google-play-store.png"
+            alt="Google Play Store"
+            className="w-full h-8 m-0"
+          />
+          <Button className="">Download from Google Play Store</Button>
         </div>
       </div>
     </div>
   );
-};
+};  
 
 export default AddFile;
